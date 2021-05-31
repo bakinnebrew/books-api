e27a2f421a87efc52892276a5901b08a9bc25a81
Ben Kinnebrew <69003238+bakinnebrew@users.noreply.github.com>
2021-05-31 15:59:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const env = process.env;
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
     host: env.DB_HOST || 'freedb.tech',
-    user: env.DB_USER || 'freedbtech_bakinnebrew',
-    password: env.DB_PASSWORD || 'Stoph4ckingme!',
-    database: env.DB_NAME || 'freedbtech_librarybooks',
+    user: env.DB_USER || '',
+    password: env.DB_PASSWORD || '',
+    database: env.DB_NAME || '',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
